7f96d81e51c36fec706a8bdf95fd825b8baa2d5e
JosephHahanyu <66301578+JosephHahanyu@users.noreply.github.com>
2020-06-02 18:04:59 +0300

    Update display.php

--- display.php
+++ display.php
@@ -4,11 +4,10 @@
         $password = "Ysr9sGuNra";
         $dbname = "sql9345016";
 
-        // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
+
+       if ($conn->connect_error) {
+            die("Connection to database failed: " . $conn->connect_error);
         } 
         $string = file_get_contents("http://geodata.grid.unep.ch/api/countries/KE/variables/331");
         $json = json_decode($string, true);
@@ -27,7 +26,6 @@ if ($result->num_rows > 0) {
 } else {
   echo "0 results";
 }
-$conn->close();
 
         $conn->close();
         ?>
