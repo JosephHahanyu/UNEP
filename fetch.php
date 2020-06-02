6becd9c970e1c90bce3895b78e35a00b10cf5db5
JosephHahanyu <66301578+JosephHahanyu@users.noreply.github.com>
2020-06-02 18:02:47 +0300

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -11,19 +11,20 @@
             die("Connection failed: " . $conn->connect_error);
         } 
         $string = file_get_contents("http://geodata.grid.unep.ch/api/countries/KE/variables/331");
-        $json = json_decode($string, true);
+        $isoarray = json_decode($string, true);
 
-
-        foreach($json['rates'] as $date =>$conversion){
-            $sql = "INSERT INTO iso (iso, year, value)
-                    VALUES ( '$iso-2', $year,'$value')";
+        foreach ($isoarray as $iso) {
+            $testAnswers .= $question['name'] . ": " . $question['value'] . "\n";
+                 $sql = "INSERT INTO iso (iso, year, value)
+                    VALUES ($iso['iso-2'], $iso['year'],$iso['value'])";
 
             if ($conn->query($sql) === TRUE) {
                 echo "New record created successfully"."<br>";
             } else {
                 echo "Error: " . $sql . "<br>" . $conn->error."<br>";
             }
-
         }
+
+        
         $conn->close();
         ?>
